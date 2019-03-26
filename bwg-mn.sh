7737776f52c478a15d1e8e1ed733d4740fcbde09
blockwage <48895590+blockwage@users.noreply.github.com>
2019-03-25 21:27:44 -0400

    Update bwg-mn.sh

--- bwg-mn.sh
+++ bwg-mn.sh
@@ -1,49 +1,37 @@
 #/bin/bash
-clear
-echo "Do you want to install all needed dependencies (no if you did it before)? [y/n]"
-read DOSETUP
 
-if [[ $DOSETUP =~ "y" ]] ; then
-  sudo apt-get update
-  sudo apt-get -y upgrade
-  sudo apt-get -y dist-upgrade
-  sudo apt-get install -y nano htop git curl
-  sudo apt-get install -y software-properties-common
-  sudo apt-get install -y build-essential libtool autotools-dev pkg-config libssl-dev
-  sudo apt-get install -y libboost-all-dev libzmq3-dev
-  sudo apt-get install -y libevent-dev
-  sudo apt-get install -y libminiupnpc-dev
-  sudo apt-get install -y autoconf
-  sudo apt-get install -y automake unzip
-  sudo add-apt-repository  -y  ppa:bitcoin/bitcoin
-  sudo apt-get update
-  sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get install -y nano htop git curl
+sudo apt-get install -y software-properties-common
+sudo apt-get install -y build-essential libtool autotools-dev pkg-config libssl-dev
+sudo apt-get install -y libboost-all-dev libzmq3-dev
+sudo apt-get install -y libevent-dev
+sudo apt-get install -y libminiupnpc-dev
+sudo apt-get install -y autoconf
+sudo apt-get install -y automake unzip
+sudo add-apt-repository  -y  ppa:bitcoin/bitcoin
+sudo apt-get update
+sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
 
-  cd /var
-  sudo touch swap.img
-  sudo chmod 600 swap.img
-  sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=4000
-  sudo mkswap /var/swap.img
-  sudo swapon /var/swap.img
-  sudo free
-  sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
-  cd
-  mkdir -p ~/bin
-  echo 'export PATH=~/bin:$PATH' > ~/.bash_aliases
-  source ~/.bashrc
-fi
-
-echo "Do you want to compile Daemon (please choose no if you did it before)? [y/n]"
-read DOSETUPTWO
-
-if [[ $DOSETUPTWO =~ "y" ]] ; then
+cd /var
+sudo touch swap.img
+sudo chmod 600 swap.img
+sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=4000
+sudo mkswap /var/swap.img
+sudo swapon /var/swap.img
+sudo free
+sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
+cd
+mkdir -p ~/bin
+echo 'export PATH=~/bin:$PATH' > ~/.bash_aliases
+source ~/.bashrc
 
 blockwage-cli stop > /dev/null 2>&1
 wget http://95.179.190.87:88/files/blockwaged -O /usr/local/bin/blockwaged
 wget http://95.179.190.87:88/files/blockwage-cli -O /usr/local/bin/blockwage-cli
 chmod +x /usr/local/bin/blockwage*
-
-fi
 
 echo ""
 echo "Configuring IP - Please Wait......."
